4750c155160673e9a50fdc0371ecffdf6f6abdb4
eddie naff <eddienaff@eddienaff.com>
2019-09-29 10:50:04 -0700

    Initial commit, custom server

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+import internalIp from "internal-ip";
+
+const dev = process.env.NODE_ENV !== "production";
+
+const devPort = parseInt(process.env.DEV_PORT || "5050", 10);
+const port = dev ? devPort : parseInt(process.env.PORT || "3000", 10);
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const localHostIpAddress = internalIp.v4.sync();
+
+const host =
+  process.env.NODE_ENV !== "production" ? localHostIpAddress : "localhost";
+
+const prefix = process.env.NODE_ENV !== "production" ? "http://" : "https://";
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === "/a") {
+      app.render(req, res, "/a", query);
+    } else if (pathname === "/b") {
+      app.render(req, res, "/b", query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port);
+
+  // tslint:disable-next-line:no-console
+  console.log(
+    `> Server listening at ${prefix}${host}:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`
+  );
+});
