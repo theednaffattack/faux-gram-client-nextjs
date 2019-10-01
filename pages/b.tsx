8184d54efd49f717f12bb8c3e3052dfe1755c356
eddie naff <eddienaff@eddienaff.com>
2019-09-30 20:21:08 -0700

    Attempt dynamic routing

--- pages/b.tsx
+++ pages/b.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { NextPage } from "next";
+import { ParsedUrlQuery } from "querystring";
 
-export default () => (
+type Props = {
+  pathname: string;
+  query: ParsedUrlQuery;
+};
+
+const BPage: NextPage<Props> = ({ pathname, query }) => (
   <Layout title="b">
     <div>b</div>
+    <div>{pathname}</div>
   </Layout>
 );
+
+BPage.getInitialProps = async ({ pathname, query }) => {
+  // pid = 'hello-nextjs'
+  const { pid } = query;
+
+  // const postContent = await fetch(
+  //   `https://api.example.com/post/${encodeURIComponent(pid)}`
+  // ).then(r => r.text())
+
+  // return { postContent }
+  return { pathname, query };
+};
+
+export default BPage;
---
+++ pages/pages/post/[pid].tsx
@@ -0,0 +1,41 @@
+// import { useRouter } from "next/router";
+import { NextPage } from "next";
+
+type Props = {
+  pathname: string;
+  query: ParsedUrlQuery;
+  pid?: string | string[];
+};
+
+import Layout from "../../../components/Layout";
+import { Header } from "../../../components/Header";
+import { ParsedUrlQuery } from "querystring";
+
+const Comment: NextPage<Props> = ({ pathname, pid, query }: Props) => {
+  // const router = useRouter();
+  // const { id, comment } = router.query;
+
+  return (
+    <Layout>
+      <Header />
+      <h1>Post: {JSON.stringify({ pid })}</h1>
+      <h1>Post: {JSON.stringify({ pathname })}</h1>
+      <h1>Post: {JSON.stringify({ query })}</h1>
+      {/* <h1>Comment: {comment}</h1> */}
+    </Layout>
+  );
+};
+
+Comment.getInitialProps = async ({ pathname, query }) => {
+  // pid = 'hello-nextjs'
+  const { pid } = query;
+
+  // const postContent = await fetch(
+  //   `https://api.example.com/post/${encodeURIComponent(pid)}`
+  // ).then(r => r.text())
+
+  // return { postContent }
+  return { pathname, query, pid };
+};
+
+export default Comment;
