4a672475fefaebf4c337b18c98fb4b3ac6dc464c
eddie naff <eddienaff@eddienaff.com>
2019-10-01 13:22:43 -0700

    Experiment with emotion and TS

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
-export const Header: React.FunctionComponent = () => (
+type Props = {
+  title?: string;
+};
+
+export const Header: React.FunctionComponent<Props> = ({ title }) => (
   <header>
     <nav>
       <Link href="/">
