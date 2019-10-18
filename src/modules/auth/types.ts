7788f80b10adf1c7dcddf626801f408c3a6ba7cf
eddie naff <eddienaff@eddienaff.com>
2019-10-18 10:35:16 -0700

    Add page types to auth module

--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -1,4 +1,36 @@
+import { ParsedUrlQuery } from "querystring";
+import {
+  BorderRadiusProps,
+  BoxShadowProps,
+  SpaceProps,
+  WidthProps,
+  HeightProps
+} from "styled-system";
+import { BoxProps, ButtonProps } from "rebass";
+
 export interface LoginCredentials {
   [email: string]: string;
   password: string;
 }
+
+export interface IPageProps {
+  pathname?: string;
+  query?: ParsedUrlQuery;
+}
+
+export interface IUserPageProps extends IPageProps {
+  token?: string | string[];
+}
+
+export interface ICardProps
+  extends BorderRadiusProps,
+    BoxProps,
+    BoxShadowProps {}
+
+export interface IButtonProps
+  extends ButtonProps,
+    BoxShadowProps,
+    BorderRadiusProps,
+    SpaceProps,
+    WidthProps,
+    HeightProps {}
