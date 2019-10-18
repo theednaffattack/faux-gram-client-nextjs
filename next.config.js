2c4732174b6c583d44e6b3befe95fa613678af2b
eddie naff <eddienaff@eddienaff.com>
2019-10-18 10:11:01 -0700

    Add bundle anaylzer and CSS loading

--- next.config.js
+++ next.config.js
@@ -3,20 +3,73 @@ require("dotenv").config();
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  webpack: config => {
-    config.plugins = config.plugins || [];
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
-    config.plugins = [
-      ...config.plugins,
+const withCSS = require("@zeit/next-css");
 
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, ".env"),
-        systemvars: true
-      })
-    ];
+const nextConfig = {
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  },
 
+  webpack(config) {
     return config;
   }
 };
+
+function HACK_removeMinimizeOptionFromCssLoaders(config) {
+  console.warn(
+    "HACK: Removing `minimize` option from `css-loader` entries in Webpack config"
+  );
+  config.module.rules.forEach(rule => {
+    if (Array.isArray(rule.use)) {
+      rule.use.forEach(u => {
+        if (u.loader === "css-loader" && u.options) {
+          delete u.options.minimize;
+        }
+      });
+    }
+  });
+}
+
+module.exports = withBundleAnalyzer(
+  withCSS({
+    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/server.html"
+      },
+      browser: {
+        analyzerMode: "static",
+        reportFilename: "../bundles/client.html"
+      }
+    },
+    webpack: config => {
+      config.plugins = config.plugins || [];
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true
+        })
+      ];
+
+      HACK_removeMinimizeOptionFromCssLoaders(config);
+      return config;
+    }
+  })
+);
