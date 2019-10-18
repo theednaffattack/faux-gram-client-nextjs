a7cbd713e0e21a27bf530e9b94c1b0fc0208e597
eddie naff <eddienaff@eddienaff.com>
2019-10-18 10:29:48 -0700

    Standardize react import

--- src/modules/cars/Overview.tsx
+++ src/modules/cars/Overview.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React from "react";
 
 import * as T from "./types";
-import Layout from "../../components/Layout";
+import Layout from "../../components/layout";
 
 export interface CarsOverviewProps {
   cars?: T.CarList;
