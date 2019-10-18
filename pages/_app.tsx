68f11e91c01c5757a1449aaf2c62117b8a5a6227
eddie naff <eddienaff@eddienaff.com>
2019-10-18 10:08:47 -0700

    Switch to styled components ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import App from "next/app";
 import React from "react";
-import { Global, css } from "@emotion/core";
-import { ThemeProvider } from "theme-ui";
+// import { Global, css } from "@emotion/core";
+// import { ThemeProvider } from "theme-ui";
 import { theme } from "../src/styles/theme";
 
 import { ApolloProvider } from "react-apollo";
-// import { ThemeProvider } from "styled-components";
-// import { createGlobalStyle } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import withApollo from "../src/lib/withApollo";
 import "./empty.css";
+
+const GlobalStyles = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  text-size-adjust: 100%;
+  font-family: "Montserrat", sans-serif;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+#__next { min-height: 100vh; display: flex }
+`;
 
 class MyApp extends App<any> {
   render() {
@@ -17,23 +34,7 @@ class MyApp extends App<any> {
     return (
       <ThemeProvider theme={theme}>
         <ApolloProvider client={apolloClient}>
-          <Global
-            styles={css`
-              html {
-                box-sizing: border-box;
-              }
-              body {
-                margin: 0;
-                text-size-adjust: 100%;
-                font-family: "Montserrat", sans-serif;
-              }
-              *,
-              *:before,
-              *:after {
-                box-sizing: inherit;
-              }
-#__next { min-height: 100vh; display: flex }
-`;
+          <GlobalStyles />
           <Component {...pageProps} />
         </ApolloProvider>
       </ThemeProvider>
