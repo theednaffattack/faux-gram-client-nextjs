52be4833c53e7edcf8ca32a04eebb1615ba9e5cf
eddie naff <eddienaff@eddienaff.com>
2019-10-18 17:24:50 -0700

    Change messages to be dynamic

---
+++ pages/messages/[id].tsx
@@ -0,0 +1,42 @@
+import { NextPage } from "next";
+
+import Layout from "../../src/components/layout";
+import GetMessagesByThreadIdPage from "../../src/components/messages/get-messages-by-thread-id-page";
+import { IMessagesPageProps } from "./types";
+
+const MessagesById: NextPage<IMessagesPageProps> = ({ id }) => {
+  let preppedId: string;
+  if (id && id.constructor === Array) {
+    preppedId = id[0];
+  }
+  if (id && typeof id === "string") {
+    preppedId = id;
+  } else {
+    preppedId = "";
+  }
+  return (
+    <Layout>
+      <GetMessagesByThreadIdPage
+        threadIdSelected={preppedId}
+        handleCloseThread={() =>
+          console.log("handleCloseThread passed as prop")
+        }
+        me=""
+        handleDisplayMessages={() =>
+          console.log("handleDisplayMessages passed as prop")
+        }
+        formDisabled={false}
+      />
+    </Layout>
+  );
+};
+
+MessagesById.getInitialProps = async ({ pathname, query }) => {
+  const { id } = query;
+
+  console.log({ pathname, query, id });
+
+  return { pathname, query, id };
+};
+
+export default MessagesById;
