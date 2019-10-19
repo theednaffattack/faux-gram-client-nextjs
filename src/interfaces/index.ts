39eb591291450a49cc5176d692047cefbcff5635
eddie naff <eddienaff@eddienaff.com>
2019-10-19 16:34:36 -0700

    Reorganize types for MyContext

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,10 +1,11 @@
-// You can include shared interfaces/types in a separate file
-// and then use them in any component by importing them. For
-// example, to import the interface below do:
-//
-// import User from 'path/to/interfaces';
+import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
+import { NextPageContext } from "next";
 
 export type User = {
-  id: number
-  name: string
+  id: number;
+  name: string;
+};
+
+export interface MyContext extends NextPageContext {
+  apolloClient: ApolloClient<NormalizedCacheObject>;
 }
