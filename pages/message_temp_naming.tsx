665ac37e83a364bb51e643e48b0ff6f0d836a9ec
eddie naff <eddienaff@eddienaff.com>
2019-10-18 17:23:44 -0700

    Convert messages to dynamic routing

---
+++ pages/message_temp_naming.tsx
@@ -0,0 +1,10 @@
+import Layout from "../src/components/layout";
+import GetOnlyThreads from "../src/components/get-only-threads_2";
+
+const GetOnlyThreadsPage = () => (
+  <Layout title="Messages">
+    <GetOnlyThreads />
+  </Layout>
+);
+
+export default GetOnlyThreadsPage;
