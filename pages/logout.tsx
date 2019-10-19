9be264bbf66a7aac80d2f3705ce265fec0e5cb9a
eddie naff <eddienaff@eddienaff.com>
2019-10-19 16:32:52 -0700

    Update our logout process

---
+++ pages/logout.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+
+import redirect from "../src/lib/redirect";
+import { LogoutDocument } from "../src/components/generated/apollo-graphql";
+import { MyContext } from "../src/interfaces";
+
+interface Props {}
+
+const Logout: NextPage<Props> = () => {
+  return null;
+};
+
+Logout.getInitialProps = async ({ apolloClient, ...ctx }: MyContext) => {
+  await apolloClient.mutate({
+    mutation: LogoutDocument
+  });
+  await apolloClient.resetStore();
+
+  redirect(ctx, "/login", { message: "You have been logged out" });
+
+  return {};
+};
+
+export default Logout;
