927f2258760cc3b8256097f4dd11921be5d3605d
eddie naff <eddienaff@eddienaff.com>
2019-10-21 16:25:32 -0700

    Add disabling of bodyscroll to the top level container

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,95 +1,73 @@
 import React from "react";
+import { disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
+
 import Layout from "../../src/components/layout";
-import { Flex, AbFlex } from "../../src/components/styled-rebass";
+import { AbFlex, Flex } from "../../src/components/styled-rebass";
 import { MeComponent } from "../../src/components/generated/apollo-graphql";
-import { TabViewer } from "../../src/modules/feed/tab-viewer";
-// import InfiniteLoader from "../../src/modules/feed/feed-infinite-loader";
+import { FollowingPostsWrapper } from "../../src/modules/feed/following-posts-container";
 
-// @ts-ignore
-const items = new Array(45)
-  .fill(undefined)
-  // @ts-ignore
-  .map((fakeItem: any, index: number) => (
-    <Flex
-      key={index}
-      // width={1 / 2}
-      ml={1}
-      mr={2}
-      p={3}
-      my={2}
-      sx={{
-        boxShadow: "0 2px 6px rgba(0, 0, 0, 0.25 )",
-        borderTop: "1px solid #eee"
-      }}
-    >
-      Item No: {index}
-    </Flex>
-  ));
+class Feed extends React.Component<object, object> {
+  listRef: React.RefObject<HTMLDivElement>;
+  constructor(props: object) {
+    super(props);
+    this.listRef = React.createRef();
+  }
 
-const Feed = () => {
-  return (
-    <Layout title="Feed">
-      <Flex bg="#eee">Some feed stuff</Flex>
-      <MeComponent>
-        {({ data: dataMe, error: errorMe, loading: loadingMe }) => {
-          if (errorMe) return <div>{JSON.stringify(errorMe)}</div>;
-          if (loadingMe) return <div>loading "Me" data...</div>;
-          const missingDataErrorMessage = {
-            __typename: "user-facing-error",
-            message: "Oh no!\nThere doesn't appear to be any data"
-          };
-          const dataConfirmed = dataMe ? dataMe : missingDataErrorMessage;
+  componentDidMount() {
+    if (this.listRef.current) {
+      console.log("THE REF APPEARS TO BE ASSIGNED", this.listRef.current);
 
-          if (dataConfirmed.__typename === "user-facing-error")
-            return <div>{dataConfirmed.message}</div>;
-          return (
-            <Flex p={0} flex="1 1 auto" style={{ position: "relative" }}>
-              <AbFlex
-                // flex="1 1 auto"
-                top={0}
-                bottom={0}
-                right={0}
-                left={0}
-                overflow="auto"
-                position="absolute"
-                flexDirection="column"
-                alignItems="stretch"
-                width={1}
-              >
-                {dataConfirmed &&
-                dataConfirmed.__typename === "user-facing-error" ? (
-                  <Flex
-                    alignItems="center"
-                    justifyContent="center"
-                    flexDirection="column"
-                    width={[1]}
-                  >
-                    {dataConfirmed.message}
-                  </Flex>
-                ) : (
-                  <TabViewer
-                    dataMe={dataMe}
-                    errorMe={errorMe}
-                    loadingMe={loadingMe}
-                  />
-                )}
-                {/* <CardList /> */}
-                {/* <InfiniteLoader
-                  fetchMore={() => console.log}
-                  imageModalState="closed"
-                  items={[1, 2, 3]}
-                  pageInfo={{}}
-                  me={dataConfirmed}
-                  toggleImageModal={() => console.log}
-                  loadingGetMessagesByThreadId={() => console.log}
-                /> */}
-              </AbFlex>
-            </Flex>
-          );
-        }}
-      </MeComponent>
-    </Layout>
-  );
-};
+      disableBodyScroll(this.listRef.current);
+    }
+  }
+  componentWillUnmount() {
+    clearAllBodyScrollLocks();
+  }
+
+  render() {
+    return (
+      <Layout title="Feed">
+        <Flex bg="#eee">Some feed stuff</Flex>
+        <MeComponent>
+          {({ data: dataMe, error: errorMe, loading: loadingMe }) => {
+            if (errorMe) return <div>{JSON.stringify(errorMe)}</div>;
+            if (loadingMe) return <div>loading "Me" data...</div>;
+            const missingDataErrorMessage = {
+              __typename: "user-facing-error",
+              message: "Oh no!\nThere doesn't appear to be any data"
+            };
+            const dataConfirmed = dataMe ? dataMe : missingDataErrorMessage;
+
+            if (dataConfirmed.__typename === "user-facing-error")
+              return <div>{dataConfirmed.message}</div>;
+            return (
+              <Flex p={0} flex="1 1 auto" style={{ position: "relative" }}>
+                <AbFlex
+                  // flex="1 1 auto"
+                  id="absolute-scroll-list"
+                  ref={this.listRef}
+                  top={0}
+                  bottom={0}
+                  right={0}
+                  left={0}
+                  overflow="auto"
+                  position="absolute"
+                  flexDirection="column"
+                  alignItems="stretch"
+                  width={1}
+                  style={{
+                    WebkitOverflowScrolling: "touch"
+                  }}
+                >
+                  <FollowingPostsWrapper />
+                </AbFlex>
+              </Flex>
+            );
+          }}
+        </MeComponent>
+      </Layout>
+    );
+  }
+}
 
 export default Feed;
