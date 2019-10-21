f986c3eb2c0e05bf619a87a4a6941b5ff9c5a041
eddie naff <eddienaff@eddienaff.com>
2019-10-21 16:29:38 -0700

    Convert to class to try and stop scrolling, no longer needed

--- src/modules/feed/following-lists-posts-only.tsx
+++ src/modules/feed/following-lists-posts-only.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import posed, { PoseGroup } from "react-pose";
+import posed from "react-pose";
 
 import { Box, Card, Flex, Heading, Text } from "../../components/styled-rebass";
 
 import UnFollowButton from "./unfollow-button";
 import WrappedAvatar from "./wrapped-avatar";
+// import { disableBodyScroll } from "body-scroll-lock";
 
 // const Box = styled(BoxBase)`
 //   ${borders}
@@ -32,82 +33,93 @@ const PosedCard = posed(Card)({
   invisible: { opacity: 0 }
 });
 
-export const FollowingList = ({ data }: IFollowingListProps) => (
-  <Flex width={1} justifyContent="center" flexDirection="row" flexWrap="wrap">
-    <PoseGroup
-      delta={1}
-      preEnterPose="invisible"
-      enterPose="enter"
-      exitPose="exit"
-      animateOnMount={true}
-      flipMove={false}
-    >
-      {data.myFollowingPosts &&
-        data.myFollowingPosts.map((post: any, pIndex: number) => (
-          <PosedCard
-            index={pIndex}
-            pose="enter"
-            bg="white"
-            key={`${pIndex} - ${post.title}`}
-            my={[3, 3, 3]}
-            mx={[3, 3, 3]}
-            sx={{
-              borderRadius: "15px",
-              boxShadow: "0 0 16px rgba(0, 0, 0, .25)"
-            }}
-            width={[1, "350px", "350px"]}
-            // border="lime"
-            display="flex"
-            style={{ opacity: 0, overflow: "hidden" }}
-          >
-            <Flex width={[1, 1, 1]} flexDirection="column">
-              <Box
-                width={[1, 1, 1]}
-                style={{
-                  minHeight: "250px",
-                  maxHeight: "250px",
-                  overflow: "hidden",
-                  backgroundImage: post.images[0]
-                    ? `url(${post.images[0].uri})`
-                    : " ",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-              />
-              <Box p={[3, 3, 3]} pt={[1, 1, 2]}>
-                <Flex alignItems="center">
-                  <Heading mr="auto">{post.title}</Heading>
+export class FollowingList extends React.Component<
+  IFollowingListProps,
+  object
+> {
+  listRef: React.RefObject<HTMLDivElement>;
+  constructor(props: IFollowingListProps) {
+    super(props);
+    this.listRef = React.createRef();
+  }
+  render() {
+    const { data } = this.props;
 
-                  <Flex
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                  >
-                    <Flex flexDirection="row" my={2} p={3} width={1}>
-                      <WrappedAvatar
-                        flexInstruction="column"
-                        user={post.user}
-                        buttonThing={false}
-                        color="blue"
-                        handleRemoveInviteeToThread={console.log}
-                      />
-                    </Flex>
-                    <UnFollowButton
-                      oldData={data}
-                      followingId={post.user.id}
-                      bg="fuchsia"
-                      me=""
+    return (
+      <Flex
+        width={1}
+        justifyContent="center"
+        flexDirection="row"
+        flexWrap="wrap"
+        border="lime"
+      >
+        {data.myFollowingPosts &&
+          data.myFollowingPosts.map((post: any, pIndex: number) => (
+            <PosedCard
+              index={pIndex}
+              pose="enter"
+              bg="white"
+              key={`${pIndex} - ${post.title}`}
+              my={[3, 3, 3]}
+              mx={[3, 3, 3]}
+              sx={{
+                borderRadius: "15px",
+                boxShadow: "0 0 16px rgba(0, 0, 0, .25)"
+              }}
+              width={[1, "350px", "350px"]}
+              // border="lime"
+              display="flex"
+              style={{ opacity: 0, overflow: "hidden" }}
+            >
+              <Flex width={[1, 1, 1]} flexDirection="column">
+                <Box
+                  width={[1, 1, 1]}
+                  style={{
+                    minHeight: "250px",
+                    maxHeight: "250px",
+                    overflow: "hidden",
+                    backgroundImage: post.images[0]
+                      ? `url(${post.images[0].uri})`
+                      : " ",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                />
+                <Box p={[3, 3, 3]} pt={[1, 1, 2]}>
+                  <Flex alignItems="center">
+                    <Heading mr="auto">{post.title}</Heading>
+
+                    <Flex
+                      justifyContent="center"
+                      alignItems="center"
+                      flexDirection="column"
                     >
-                      unfollow
-                    </UnFollowButton>
+                      <Flex flexDirection="row" my={2} p={3} width={1}>
+                        <WrappedAvatar
+                          flexInstruction="column"
+                          user={post.user}
+                          buttonThing={false}
+                          color="blue"
+                          handleRemoveInviteeToThread={console.log}
+                        />
+                      </Flex>
+                      <UnFollowButton
+                        oldData={data}
+                        followingId={post.user.id}
+                        bg="fuchsia"
+                        me=""
+                      >
+                        unfollow
+                      </UnFollowButton>
+                    </Flex>
                   </Flex>
-                </Flex>
-                <Text alignSelf="end">{post.text}</Text>
-              </Box>
-            </Flex>
-          </PosedCard>
-        ))}
-    </PoseGroup>
-  </Flex>
-);
+                  <Text alignSelf="end">{post.text}</Text>
+                </Box>
+              </Flex>
+            </PosedCard>
+          ))}
+      </Flex>
+    );
+  }
+}
