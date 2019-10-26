54dd72e1f6f313ce42054b02cca16f8a86804199
eddie naff <eddienaff@eddienaff.com>
2019-10-25 18:50:29 -0700

    Add types

--- src/modules/feed/feed-card.tsx
+++ src/modules/feed/feed-card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { clearAllBodyScrollLocks, disableBodyScroll } from "body-scroll-lock";
+import Link from "next/link";
 
 import {
   Box,
@@ -9,8 +10,12 @@ import {
   Heading,
   Text
 } from "../../components/styled-rebass";
+import Icon from "../icon/icon";
+import { IPageProps } from "../../page-types/types";
+import { User } from "../../interfaces";
+import UnFollowUserButtonGqlWrapper from "./unfollow-user-button-gql-wrapper";
 
-type TImage = {
+export type TImage = {
   id: string;
   uri: string;
   __typename: string;
@@ -18,47 +23,91 @@ type TImage = {
 
 /**
  * ISingleFeedCardProps
- * @param image string;
+ * @param id ID;
+ * @param TImage [Image];
+ * @param title string;
+ * @param description string;
  */
-interface ISingleFeedCardProps {
+export interface ISingleFeedCardProps extends IPageProps {
+  /**
+   * ISingleFeedCardProps
+   * @param image string;
+   */
+
+  /** The ID of the user who created the Post */
+  postUserId: User["id"] | string;
   /** image URI */
-  images: TImage[];
+  id: string;
+  /** image URI */
+  images: any[];
   /** title given by user */
   title: string;
   /** image description of the image */
   description: string;
-  /** graphql schema type */
-  // __typename?: string;
+  /** pageProps from top-level getInitialProps */
 }
 
 export const FeedCard: React.FunctionComponent<ISingleFeedCardProps> = ({
+  id,
   images,
+  description,
   title,
-  description
+  pathname,
+  postUserId
 }) => {
+  const isThisADynamicRoute =
+    pathname && pathname[pathname.length - 1] === "]" ? true : false;
   return (
-    <Box width={1} px={3} py={2}>
-      <Card
-        width={1}
-        sx={{
-          p: 1,
-          borderRadius: 2,
-          boxShadow: "0 0 16px rgba(0, 0, 0, .25)"
-        }}
-      >
-        {images && images.length > 0 ? <Image src={images[0].uri} /> : null}
-        <Box px={2}>
-          <Heading as="h3">{title}</Heading>
-          <Text fontSize={0}>{description}</Text>
+    <Card
+      px={3}
+      py={2}
+      ml={2}
+      my={2}
+      width={0.98}
+      sx={{
+        p: 1,
+        borderRadius: 18,
+        boxShadow: "0 0 16px rgba(0, 0, 0, .25)"
+        // minHeight: "100%"
+        // border: "2px pink solid"
+        // display: "flex",
+        // flex: "1 1 auto"
+      }}
+    >
+      <Flex>
+        <Box ml="auto">
+          <Link href={isThisADynamicRoute === true ? "/feed" : `/feed/${id}`}>
+            <a>
+              <Icon
+                name={isThisADynamicRoute ? "close" : "expand"}
+                fill="fuchsia"
+                size="3em"
+              />
+            </a>
+          </Link>
         </Box>
-      </Card>
-    </Box>
+      </Flex>
+
+      {images && images.length > 0 ? (
+        <Image width={["300px", "600px"]} src={images[0].uri} />
+      ) : null}
+      <Box px={2}>
+        <Heading as="h3">{title}</Heading>
+        <Text fontSize={0}>{description}</Text>
+      </Box>
+      <Flex border="lime">
+        <Icon name="heart" fill="fuchsia" size="3em" />
+
+        <Icon name="chat" fill="fuchsia" size="3em" />
+        <UnFollowUserButtonGqlWrapper postUserId={postUserId as string} />
+      </Flex>
+    </Card>
   );
 };
 
 export default FeedCard;
 
-interface IFeedCardsProps {
+interface IFeedCardsProps extends IPageProps {
   cardData: ISingleFeedCardProps[];
 }
 
@@ -79,15 +128,17 @@ export class FeedCards extends React.Component<IFeedCardsProps> {
     clearAllBodyScrollLocks();
   }
   render() {
-    const { cardData } = this.props;
+    const { cardData, pathname, query } = this.props;
 
     return (
       <Flex flexDirection="column" flex="1 1 auto" ref={this.listRef}>
         {cardData.map((card, index) => {
-          console.log("VIEW CARD DATA", card);
-
           return (
             <FeedCard
+              pathname={pathname}
+              postUserId={card.postUserId}
+              query={query}
+              id={card.id}
               key={`card-${index}`}
               images={card.images}
               title={card.title}
