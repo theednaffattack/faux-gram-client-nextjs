77174ad80250512bc38da7c42847ec014696eda1
eddie naff <eddienaff@eddienaff.com>
2019-10-25 18:42:19 -0700

    Remove cache.writeQuery to followingPosts to remove error

--- src/components/follow-button.tsx
+++ src/components/follow-button.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import { Button } from "rebass";
+import { ApolloError } from "apollo-boost";
 
-import { IFollowButtonProps } from "./types";
+import { Button } from "../components/styled-rebass";
 import { GET_GLOBAL_POSTS } from "../graphql/user/queries/GetGlobalPosts";
-import { MY_FOLLOWING_POSTS } from "../graphql/user/queries/MyFollowingPosts";
+import {
+  FollowUserMutationFn,
+  FollowUserMutationResult,
+  User,
+  GetGlobalPostsProps
+} from "./generated/apollo-graphql";
+
+type HandleMutationClickProps = FollowUserMutationFn;
+
+export interface IFollowButtonProps {
+  isCtxUserIdAFollower: boolean;
+  data?: FollowUserMutationResult["data"];
+  children: any;
+  followUser: FollowUserMutationFn;
+  me: User;
+  postUserId: User["id"];
+  errorGlblPosts: FollowUserMutationResult["error"];
+}
 
 export default class FollowButton extends React.Component<
   IFollowButtonProps,
@@ -14,64 +31,88 @@ export default class FollowButton extends React.Component<
 
     this.handleMutationClick = this.handleMutationClick.bind(this);
   }
-  handleMutationClick({ followUser }: any) {
-    try {
-      followUser({
-        variables: {
-          data: {
-            userIDToFollow: this.props.postUserId
-          }
-        },
-        update: (cache: any, { data, error }: any) => {
-          if (error) console.error("Follow Error!", error);
+  async handleMutationClick(followUser: HandleMutationClickProps) {
+    await followUser({
+      variables: {
+        data: {
+          userIDToFollow: this.props.postUserId
+        }
+      },
+      update: (cache, { data, errors }) => {
+        if (errors)
+          console.error("Follow Error!", JSON.stringify(errors, null, 2));
 
-          // server returns false if "me" is already
-          // following the specified user, so we return to disallow
-          // it quickly. Another check for this happens below
-          if (!data || !data.followUser) {
-            return;
-          }
+        // server returns false if "me" is already
+        // following the specified user, so we return to disallow
+        // it quickly. Another check for this happens below
+        if (!data || !data.followUser) {
+          return;
+        }
 
-          // Read the data from our cache for this query.
-          const storeUpdateData = cache.readQuery({
-            query: MY_FOLLOWING_POSTS
-          });
+        const globalPostData: any = cache.readQuery({
+          query: GET_GLOBAL_POSTS
+        });
 
-          const globalPostData = cache.readQuery({
-            query: GET_GLOBAL_POSTS
-          });
-
-          let getNewItems_too = globalPostData.getGlobalPosts.filter(
-            (item: any) => {
-              return item.user.id === this.props.postUserId;
-            }
-          );
-
-          cache.writeQuery({
-            query: MY_FOLLOWING_POSTS,
-            data: {
-              myFollowingPosts: [
-                ...storeUpdateData.myFollowingPosts,
-                ...getNewItems_too
+        const addUserToThoseIFollow = globalPostData
+          ? {
+              getGlobalPosts: [
+                ...globalPostData.getGlobalPosts.map((item: any) => {
+                  if (item.user.id === this.props.postUserId) {
+                    return { ...item, isCtxUserIdAFollowerOfPostUser: true };
+                  } else {
+                    return item;
+                  }
+                })
               ]
             }
-          });
-        }
-      });
-    } catch (error) {
-      console.log("oh no an error", error);
-    }
+          : [];
+
+        cache.writeQuery({
+          query: GET_GLOBAL_POSTS,
+          data: addUserToThoseIFollow
+        });
+
+        // // Read the data from our cache for this query.
+        // const storeUpdateData = cache.readQuery({
+        //   query: MY_FOLLOWING_POSTS
+        // });
+        // console.log("WHAT IS STORE UPDATE DATA?", storeUpdateData);
+
+        // let getNewItems_too = globalPostData.getGlobalPosts.filter(
+        //   (item: any) => {
+        //     return item.user.id === this.props.postUserId;
+        //   }
+        // );
+
+        // console.log("BEFORE WRITING TO CACHE...", {
+        //   storeUpdateData,
+        //   globalPostData,
+        //   getNewItems_too
+        // });
+
+        // cache.writeQuery({
+        //   query: MY_FOLLOWING_POSTS,
+        //   data: {
+        //     myFollowingPosts: [
+        //       ...storeUpdateData.myFollowingPosts,
+        //       ...getNewItems_too
+        //     ]
+        //   }
+        // });
+      }
+    }).catch((error: ApolloError) => console.log("oh no an error", error));
   }
   render() {
     const { children, data, followUser, me, ...props } = this.props;
 
     return (
       <Button
+        bg="blue"
         disabled={this.props.postUserId === me.id ? true : false}
         type="button"
         onClick={() => {
           if (this.props.postUserId !== me.id) {
-            this.handleMutationClick({ followUser });
+            this.handleMutationClick(followUser);
           }
         }}
         {...props}
