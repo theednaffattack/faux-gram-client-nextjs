90092fd7ca23abfefdd19dfbfcc42f789968f281
eddie naff <eddienaff@eddienaff.com>
2019-10-25 18:39:14 -0700

    Add global feed route

---
+++ pages/feed/global.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NextPage } from "next";
+
+import Layout from "../../src/components/layout";
+import { MyContext } from "../../src/components/interfaces/my-context";
+import { IPageProps } from "../../src/page-types/types";
+import { MeComponent } from "../../src/components/generated/apollo-graphql";
+import GlobalFeed from "../../src/components/global-feed";
+// import FeedPage from "../../src/modules/feed/feed-page";
+
+const Feed: NextPage<IPageProps> = () => {
+  return (
+    <Layout title="Feed">
+      <MeComponent>
+        {({ data, error, loading }) => {
+          if (error) return <div>Error, {JSON.stringify(error)}</div>;
+          if (loading) return <div>loading...</div>;
+          return <GlobalFeed me={data} />;
+        }}
+      </MeComponent>
+    </Layout>
+  );
+};
+
+Feed.getInitialProps = async ({ pathname, query }: MyContext) => {
+  const { id } = query;
+
+  return { pathname, query, id };
+};
+
+export default Feed;
