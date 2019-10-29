905b5870e29e7e59cc539fafeacb68d950664d3c
eddie naff <eddienaff@eddienaff.com>
2019-10-29 15:17:07 -0700

    Fix module import to be above local imports

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { AbFlex, Flex } from "./styled-rebass";
 import LayoutFooter from "./layout-footer";
 import { SidebarNavigation } from "./sidebar-navigation";
