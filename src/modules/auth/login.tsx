31b1e7ca1b11c4f923b791b5106fc1ce5fa5e362
eddie naff <eddienaff@eddienaff.com>
2019-10-29 15:25:43 -0700

    Remove layout

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -21,7 +21,6 @@ import {
 import Router from "next/router";
 
 import { InputField } from "../../components/fields/input-field";
-import Layout from "../../components/layout";
 import {
   LoginComponent,
   MeQuery
@@ -87,154 +86,158 @@ const Card: StyledComponent<React.FunctionComponent<ICardProps>, any> = styled(
 
 export default () => {
   return (
-    <Layout title="Login page">
-      <Flex minHeight="100vh">
-        <InnerFlex width={[1]} minHeight="100vh">
-          <ContentFlex
-            mt={[0, 5, 0]}
-            flexDirection="column"
-            width={[1]}
-            justifyContent="center"
-            alignItems="center"
+    <Flex minHeight="100vh">
+      <InnerFlex width={[1]} minHeight="100vh">
+        <ContentFlex
+          mt={[0, 5, 0]}
+          flexDirection="column"
+          width={[1]}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Card
+            mx={3}
+            width={1}
+            maxWidth={["350px", "350px"]}
+            p={4}
+            borderRadius="10px"
+            bg="rgb(242,242,242)"
+            boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
           >
-            <Card
-              mx={3}
-              width={1}
-              maxWidth={["350px", "350px"]}
-              p={4}
-              borderRadius="10px"
-              bg="rgb(242,242,242)"
-              boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
-            >
-              <ContentFlex mt={3} mb={4} justifyContent="center">
-                <Heading color="text" fontSize={[5]} fontFamily="montserrat">
-                  Sign in
-                </Heading>
-              </ContentFlex>
-              <LoginComponent>
-                {login => (
-                  <Formik
-                    validateOnBlur={false}
-                    validateOnChange={false}
-                    onSubmit={async (data, { setErrors }) => {
-                      let response;
-                      try {
-                        response = await login({
-                          variables: data,
-                          update: (cache, { data }) => {
-                            if (!data || !data.login) {
-                              return;
-                            }
-                            cache.writeQuery<MeQuery>({
-                              query: meQuery,
-                              data: {
-                                __typename: "Query",
-                                me: data.login
-                              }
-                            });
+            <ContentFlex mt={3} mb={4} justifyContent="center">
+              <Heading color="text" fontSize={[5]} fontFamily="montserrat">
+                Sign in
+              </Heading>
+            </ContentFlex>
+            <LoginComponent>
+              {login => (
+                <Formik
+                  validateOnBlur={false}
+                  validateOnChange={false}
+                  onSubmit={async (data, { setErrors }) => {
+                    let response;
+                    try {
+                      response = await login({
+                        variables: data,
+                        update: (cache, { data }) => {
+                          if (!data || !data.login) {
+                            console.log("!DATA || !DATA.LOGIN");
+
+                            return;
                           }
-                        });
-                      } catch (error) {
-                        const displayErrors: { [key: string]: string } = {};
+                          console.log("BEFORE WRITEQUERY");
 
-                        let myErrors = error.graphQLErrors; //.extensions.exception.validationErrors;
-                        console.log(
-                          "myErrors",
-                          JSON.stringify(myErrors, null, 2)
-                        );
-                        myErrors.forEach((errorThing: any) => {
-                          displayErrors[errorThing.path[0]] =
-                            errorThing.message;
-                        });
-
-                        myErrors.forEach((validationError: any) => {
-                          Object.values(validationError.constraints).forEach(
-                            (message: any) => {
-                              displayErrors[validationError.property] = message;
+                          cache.writeQuery<MeQuery>({
+                            query: meQuery,
+                            data: {
+                              __typename: "Query",
+                              me: data.login
                             }
-                          );
-                        });
+                          });
+                          console.log("AFTER WRITEQUERY");
+                        }
+                      });
+                      Router.push("/login");
+                    } catch (error) {
+                      const displayErrors: { [key: string]: string } = {};
 
-                        return setErrors(displayErrors);
+                      let myErrors = error.graphQLErrors; //.extensions.exception.validationErrors;
+                      console.log(
+                        "myErrors",
+                        JSON.stringify(myErrors, null, 2)
+                      );
+                      myErrors.forEach((errorThing: any) => {
+                        displayErrors[errorThing.path[0]] = errorThing.message;
+                      });
 
-                        // return setErrors({
-                        //   email: "invalid login"
-                        // });
-                      }
+                      myErrors.forEach((validationError: any) => {
+                        Object.values(validationError.constraints).forEach(
+                          (message: any) => {
+                            displayErrors[validationError.property] = message;
+                          }
+                        );
+                      });
 
-                      if (response && response.data && !response.data.login) {
-                        setErrors({
-                          email: "invalid login"
-                        });
-                        return;
-                      }
+                      return setErrors(displayErrors);
 
-                      Router.push("/welcome");
-                    }}
-                    initialValues={{
-                      email: "",
-                      password: "",
-                      keepMeSigned: true
-                    }}
-                  >
-                    {({ handleSubmit }) => (
-                      <form onSubmit={handleSubmit}>
-                        <Field
-                          id="email"
-                          name="email"
-                          placeholder="input email"
-                          component={InputField}
-                        />
-                        <Field
-                          id="password"
-                          name="password"
-                          placeholder="input password"
-                          type="password"
-                          component={InputField}
-                        />
-                        <ContentFlex my={2}>
-                          <Box mr="auto">
-                            <Text
-                              htmlFor="keepMeSignedIn"
-                              fontFamily="montserrat"
-                            >
-                              Keep me logged in
-                            </Text>
-                          </Box>
-                          <Box mr={2}>
-                            <label>
-                              <Field
-                                id="keepMeSignedIn"
-                                name="keepMeSignedIn"
-                                type="checkbox"
-                                shadow="0px 10px 27px 0px rgba(0, 0, 0, 0.1)"
-                                component={CheckBox}
-                              />
-                            </label>
-                          </Box>
-                        </ContentFlex>
-                        <ContentFlex justifyContent="center">
-                          <Button
-                            mt={2}
-                            width="340px"
-                            height="47px"
-                            borderRadius="30px"
-                            boxShadow="0px 10px 27px 0px rgba(0, 0, 0, 0.1)"
-                            type="submit"
+                      // return setErrors({
+                      //   email: "invalid login"
+                      // });
+                    }
+
+                    if (response && response.data && !response.data.login) {
+                      setErrors({
+                        email: "invalid login"
+                      });
+                      return;
+                    }
+
+                    Router.push("/welcome");
+                  }}
+                  initialValues={{
+                    email: "",
+                    password: "",
+                    keepMeSigned: true
+                  }}
+                >
+                  {({ handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                      <Field
+                        id="email"
+                        name="email"
+                        placeholder="input email"
+                        type="text"
+                        component={InputField}
+                      />
+                      <Field
+                        id="password"
+                        name="password"
+                        placeholder="input password"
+                        type="password"
+                        component={InputField}
+                      />
+                      <ContentFlex my={2}>
+                        <Box mr="auto">
+                          <Text
+                            htmlFor="keepMeSignedIn"
+                            fontFamily="montserrat"
                           >
-                            <Text fontFamily="montserrat">Login</Text>
-                          </Button>
-                        </ContentFlex>
-                      </form>
-                    )}
-                  </Formik>
-                )}
-              </LoginComponent>
-            </Card>
-            <SignUpLink width={["100%", "500px"]} />
-          </ContentFlex>
-        </InnerFlex>
-      </Flex>
-    </Layout>
+                            Keep me logged in
+                          </Text>
+                        </Box>
+                        <Box mr={2}>
+                          <label>
+                            <Field
+                              id="keepMeSignedIn"
+                              name="keepMeSignedIn"
+                              type="checkbox"
+                              shadow="0px 10px 27px 0px rgba(0, 0, 0, 0.1)"
+                              component={CheckBox}
+                            />
+                          </label>
+                        </Box>
+                      </ContentFlex>
+                      <ContentFlex justifyContent="center">
+                        <Button
+                          mt={2}
+                          width="340px"
+                          height="47px"
+                          borderRadius="30px"
+                          boxShadow="0px 10px 27px 0px rgba(0, 0, 0, 0.1)"
+                          type="submit"
+                        >
+                          <Text fontFamily="montserrat">Login</Text>
+                        </Button>
+                      </ContentFlex>
+                    </form>
+                  )}
+                </Formik>
+              )}
+            </LoginComponent>
+          </Card>
+          <SignUpLink width={["100%", "500px"]} />
+        </ContentFlex>
+      </InnerFlex>
+    </Flex>
   );
 };
