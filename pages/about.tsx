c1146474274bcde7a21f7ac56a43bf0fdc71d548
eddie naff <eddienaff@eddienaff.com>
2019-10-29 15:12:24 -0700

    Add logout to test multi-tab logout

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import Layout from "../src/components/layout";
+import { Flex } from "../src/components/styled-rebass";
+import { logout } from "../src/lib/localstorage-logout";
 
 const AboutPage: React.FunctionComponent = () => (
-  <Layout title="About | Next.js + TypeScript Example">
+  <Flex flexDirection="column">
     <h1>About</h1>
     <p>This is the about page</p>
+    <button type="button" onClick={() => logout()}>
+      log me out
+    </button>
     <p>
       <Link href="/" as="/  ">
         <a>Go home</a>
       </Link>
     </p>
-  </Layout>
+  </Flex>
 );
 
 export default AboutPage;
