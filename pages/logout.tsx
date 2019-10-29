80b9ba1e327c16199200b67eb9ae17680f759ad4
eddie naff <eddienaff@eddienaff.com>
2019-10-29 15:14:08 -0700

    Call logout function to enable multi-tab logout

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -3,6 +3,8 @@ import { NextPage } from "next";
 import redirect from "../src/lib/redirect";
 import { LogoutDocument } from "../src/components/generated/apollo-graphql";
 import { MyContext } from "../src/interfaces";
+import { isBrowser } from "../src/lib/isBrowser";
+import { logout } from "../src/lib/localstorage-logout";
 
 interface Props {}
 
@@ -10,13 +12,25 @@ const Logout: NextPage<Props> = () => {
   return null;
 };
 
+// @ts-ignore
 Logout.getInitialProps = async ({ apolloClient, ...ctx }: MyContext) => {
   await apolloClient.mutate({
     mutation: LogoutDocument
   });
+
+  // await apolloClient.resetStore();
   await apolloClient.clearStore();
 
-  redirect(ctx, "/login", { message: "You have been logged out" });
+  let now = new Date();
+
+  console.log("Before logout?");
+
+  // To trigger the event listener we save some random data into the `logout` key
+  isBrowser ? window.localStorage.setItem("logout", now.toISOString()) : null; // new
+
+  isBrowser ? logout() : console.log("dammit");
+
+  redirect(ctx, "/login");
 
   return {};
 };
