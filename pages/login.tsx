cb5d30c5d175cd4bb7f7e5e4ed5d2d27a90dbe65
eddie naff <eddienaff@eddienaff.com>
2019-10-30 12:13:11 -0700

    Add getInitialProps to login to pass referer

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,20 @@
-import Login from "../src/modules/auth/login";
+import { NextPage } from "next";
 
-const LoginPage = () => <Login />;
+import Login from "../src/modules/auth/login";
+import { MyContext } from "../types/types";
+
+interface LoginProps {
+  referer: MyContext["referer"];
+}
+
+const LoginPage: NextPage<LoginProps> = ({ referer }) => (
+  <Login referer={referer} />
+);
+
+// @ts-ignore
+LoginPage.getInitialProps = async ({ referer }: MyContext) => {
+  let setReferer = referer === undefined ? "/login" : referer;
+  return { referer: setReferer };
+};
 
 export default LoginPage;
