31aa9f56c6fc1cd641b936398dbf1855baa389a0
eddie naff <eddienaff@eddienaff.com>
2019-10-30 12:14:36 -0700

    Create new shared types file at top-level

---
+++ types/types.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
+import { NextPageContext } from "next";
+
+export interface MyContext extends NextPageContext {
+  apolloClient: ApolloClient<NormalizedCacheObject>;
+  referer: string;
+}
