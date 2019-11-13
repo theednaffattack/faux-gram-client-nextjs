c96df4f113d47f25f15790cd084eda009cd8f53e
eddie naff <eddienaff@eddienaff.com>
2019-11-13 00:45:53 -0800

    Add comment page

---
+++ pages/compose/comment.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Button } from "../../src/components/styled-rebass";
+import Modal from "../../src/modules/modal/modal";
+import ToggleContent from "../../src/modules/modal/toggle-content";
+
+interface Props {
+  hello?: "hello";
+}
+
+const CreateComment: React.FunctionComponent<Props> = () => {
+  return (
+    <div
+      sx={{
+        position: "relative"
+      }}
+    >
+      <ToggleContent
+        toggle={(show: any) => (
+          <Button type="button" onClick={show}>
+            Open
+          </Button>
+        )}
+        content={(hide: any) => (
+          <Modal>
+            There is no spoon...
+            <Button type="button" onClick={hide}>
+              Close
+            </Button>
+          </Modal>
+        )}
+      >
+        <Modal />
+      </ToggleContent>
+    </div>
+  );
+};
+
+export default CreateComment;
