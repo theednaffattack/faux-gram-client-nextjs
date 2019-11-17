1c37a6cdc9b78e4ba7c1c4abca3558dc85c4d2b1
eddie naff <eddienaff@eddienaff.com>
2019-11-17 15:27:57 -0800

    Add dropzone types from Post form

--- types/types.ts
+++ types/types.ts
@@ -5,3 +5,40 @@ export interface MyContext extends NextPageContext {
   apolloClient: ApolloClient<NormalizedCacheObject>;
   referer: string;
 }
+
+export interface IDropZoneProps {
+  data: any;
+  disabled: boolean;
+  error: any;
+  loading: any;
+  me: any;
+  mutation: any;
+  onFilesAdded: any;
+  signS3: any;
+}
+
+export interface IDropZoneContainerProps {
+  mutateCreatePost: any;
+  me: any;
+  mutateSignS3: any;
+
+  dataCreatePost: any;
+  errorCreatePost: any;
+  loadingCreatePost: any;
+}
+
+// safe to delete? I think I just renamed this,
+// but can't really remember
+// export interface IDropZoneState {
+//   highlight: boolean;
+//   files: any[];
+//   name: string;
+// }
+
+export interface IDropZoneContainerState {
+  highlight: boolean;
+  files: any[];
+  name: string;
+  disabled: boolean;
+  fileInputKey: string;
+}
