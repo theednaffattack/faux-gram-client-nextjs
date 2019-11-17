26cf6cd4087f974e99f719da615fa76cd82ee47d
eddie naff <eddienaff@eddienaff.com>
2019-11-17 15:22:29 -0800

    Extract dropzone props to top-level types folder

--- src/components/types.ts
+++ src/components/types.ts
@@ -164,43 +164,6 @@ export interface IImagePreviewSingleProps {
   imageFile: any;
 }
 
-export interface IDropZoneProps {
-  data: any;
-  disabled: boolean;
-  error: any;
-  loading: any;
-  me: any;
-  mutation: any;
-  onFilesAdded: any;
-  signS3: any;
-}
-
-export interface IDropZoneContainerProps {
-  mutateCreatePost: any;
-  me: any;
-  mutateSignS3: any;
-
-  dataCreatePost: any;
-  errorCreatePost: any;
-  loadingCreatePost: any;
-}
-
-// safe to delete? I think I just renamed this,
-// but can't really remember
-// export interface IDropZoneState {
-//   highlight: boolean;
-//   files: any[];
-//   name: string;
-// }
-
-export interface IDropZoneContainerState {
-  highlight: boolean;
-  files: any[];
-  name: string;
-  disabled: boolean;
-  fileInputKey: string;
-}
-
 export interface IUnFollowButtonProps extends ButtonProps {
   me: string;
   oldData: any;
