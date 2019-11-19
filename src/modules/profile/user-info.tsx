0c16ebda4dab6b50b997d731b66c0874a889fc97
eddie naff <eddienaff@eddienaff.com>
2019-11-19 11:54:31 -0800

    Adjust user info component for editing purposes

--- src/modules/profile/user-info.tsx
+++ src/modules/profile/user-info.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { useState } from "react";
 import { Formik } from "formik";
 
-import { Button, Flex, Text } from "../../components/styled-rebass";
+import { Button, Flex, Icon, Text } from "../../components/styled-rebass";
 import UserProfileImage from "./user-profile-image";
-import { MeQueryResult } from "../../components/generated/apollo-graphql";
+import {
+  MeQueryResult,
+  EditUserInfoMutationResult,
+  EditUserInfoMutationFn,
+  MeComponent,
+  SignS3Component,
+  AddProfilePictureComponent
+} from "../../components/generated/apollo-graphql";
 import EditUserInfoFormBody from "./edit-user-info-form-body";
+import ButtonRow from "./button-row";
+import DropZoneContainer from "./dropzone-container";
 
 interface UserInfoProps {
   expanded: boolean;
@@ -13,143 +22,262 @@ interface UserInfoProps {
   dataMe: MeQueryResult["data"];
   errorMe: MeQueryResult["error"];
   loadingMe: MeQueryResult["loading"];
+  loadingSubmitState: LoadingStateProps;
+  setLoadingSubmitState: React.Dispatch<
+    React.SetStateAction<LoadingStateProps>
+  >;
+  editUserInfo: EditUserInfoMutationFn;
+  dataEditUserInfo: EditUserInfoMutationResult["data"];
+  errorEditUserInfo: EditUserInfoMutationResult["error"];
+  loadingEditUserInfo: EditUserInfoMutationResult["loading"];
 }
 
-const variables = {
-  data: "some fake data"
-};
+interface UserInfoContainerProps {
+  dataMe: MeQueryResult["data"];
+  errorMe: MeQueryResult["error"];
+  loadingMe: MeQueryResult["loading"];
+  editUserInfo: EditUserInfoMutationFn;
+  dataEditUserInfo: EditUserInfoMutationResult["data"];
+  errorEditUserInfo: EditUserInfoMutationResult["error"];
+  loadingEditUserInfo: EditUserInfoMutationResult["loading"];
+}
 
-const register = ({ variables }: any) => {
-  console.log();
-  return { response: variables };
-};
+export interface InitialValuesProps {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
 
 const UserInfo: React.FunctionComponent<UserInfoProps> = ({
-  expanded,
-  setExpanded,
   dataMe,
   errorMe,
-  loadingMe
+  expanded,
+  loadingMe,
+  loadingSubmitState,
+  setExpanded,
+  setLoadingSubmitState,
+  editUserInfo
+  // dataEditUserInfo,
+  // errorEditUserInfo,
+  // loadingEditUserInfo
 }) => {
   if (errorMe)
     return (
       <div>Error loading user data, {JSON.stringify(errorMe, null, 2)}</div>
     );
-  if (dataMe) {
+  if (dataMe && dataMe.me) {
+    let initialFormValues = {
+      email: dataMe.me.email,
+      firstName: dataMe.me.firstName,
+      lastName: dataMe.me.lastName
+    };
     return (
-      <>
-        <Flex
-          mb={3}
-          alignItems="center"
-          border={expanded ? "crimson" : "2px transparent dashed"}
-        >
-          <UserProfileImage flexInstruction="row" user={dataMe && dataMe.me} />
-
-          {expanded ? (
-            <Formik
-              validateOnBlur={false}
-              validateOnChange={false}
-              // @ts-ignore
-              onSubmit={async (data, { setErrors }) => {
-                try {
-                  const response = await register({
-                    variables: variables
-                  });
-
-                  console.log({ response });
-                } catch (error) {
-                  const displayErrors: { [key: string]: string } = {};
-
-                  let myErrors =
-                    error.graphQLErrors[0].extensions.exception
-                      .validationErrors;
-
-                  myErrors.forEach((validationError: any) => {
-                    Object.values(validationError.constraints).forEach(
-                      (message: any) => {
-                        displayErrors[validationError.property] = message;
-                      }
-                    );
-                  });
-                  return setErrors(displayErrors);
-
-                  // const errors: { [key: string]: string } = {};
-                  // err.graphQLErrors[0].validationErrors.forEach(
-                  //   (validationErr: any) => {
-                  //     Object.values(validationErr.constraints).forEach(
-                  //       (message: any) => {
-                  //         errors[validationErr.property] = message;
-                  //       }
-                  //     );
-                  //   }
-                  // );
-                  // setErrors(errors);
+      <Formik
+        validateOnBlur={false}
+        validateOnChange={false}
+        onSubmit={async (data, { setErrors }) => {
+          try {
+            await editUserInfo({
+              variables: {
+                data: {
+                  email: data.email,
+                  firstName: data.firstName,
+                  lastName: data.lastName
                 }
-              }}
-              initialValues={{
-                email: "test@test.com",
-                firstName: "test-firstName",
-                lastName: "test-lastName",
-                password: "testPassword"
-              }}
-            >
-              {({ handleSubmit, values }) => (
-                <EditUserInfoFormBody
-                  handleSubmit={handleSubmit}
-                  values={values}
-                />
-              )}
-            </Formik>
-          ) : (
-            <Flex flexDirection="column">
-              <Text fontFamily="main">
-                {loadingMe
-                  ? "loading..."
-                  : dataMe && dataMe.me && dataMe.me.name}
-              </Text>
+              }
+            });
+          } catch (error) {
+            const displayErrors: { [key: string]: string } = {};
 
-              <Text>
-                {loadingMe
-                  ? "loading..."
-                  : dataMe && dataMe.me && dataMe.me.email}
-              </Text>
+            let myErrors =
+              error.graphQLErrors[0].extensions.exception.validationErrors;
+
+            myErrors.forEach((validationError: any) => {
+              Object.values(validationError.constraints).forEach(
+                (message: any) => {
+                  displayErrors[validationError.property] = message;
+                }
+              );
+            });
+            return setErrors(displayErrors);
+          }
+        }}
+        initialValues={initialFormValues}
+      >
+        {({ handleSubmit, values, resetForm }) => (
+          <Flex flexDirection="column">
+            {loadingSubmitState === "IS_LOADING" ? "LOADING..." : ""}
+            <Flex mb={3} alignItems="center">
+              <Flex
+                width={1 / 2}
+                alignItems="center"
+                flexDirection="column"
+                sx={{ position: "relative" }}
+              >
+                <UserProfileImage
+                  flexInstruction="row"
+                  user={dataMe && dataMe.me}
+                />
+
+                {expanded ? (
+                  <Flex
+                    alignItems="center"
+                    justifyContent="center"
+                    bg="rgba(255,255,255,0.7)"
+                    sx={{
+                      // border: "2px pink dashed",
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      bottom: 0,
+                      right: 0
+                    }}
+                  >
+                    <MeComponent>
+                      {({
+                        data: dataMe,
+                        error: errorMe,
+                        loading: loadingMe
+                      }) => {
+                        return (
+                          <SignS3Component>
+                            {(
+                              signS3,
+                              {
+                                data: dataSignS3,
+                                error: errorSignS3,
+                                loading: loadingSignS3
+                              }
+                            ) => {
+                              return (
+                                <AddProfilePictureComponent>
+                                  {(
+                                    addProfilePicture,
+                                    // @ts-ignore
+                                    {
+                                      data: dataAddProfilePicture,
+                                      error: errorAddProfilePicture,
+                                      loading: loadingAddProfilePicture
+                                    }
+                                  ) => {
+                                    return (
+                                      <DropZoneContainer
+                                        dataMe={dataMe}
+                                        errorMe={errorMe}
+                                        loadingMe={loadingMe}
+                                        mutateSignS3={signS3}
+                                        dataSignS3={dataSignS3}
+                                        errorSignS3={errorSignS3}
+                                        loadingSignS3={loadingSignS3}
+                                        mutateAddProfilePicture={
+                                          addProfilePicture
+                                        }
+                                        dataAddProfilePicture={
+                                          dataAddProfilePicture
+                                        }
+                                        errorAddProfilePicture={
+                                          errorAddProfilePicture
+                                        }
+                                        loadingAddProfilePicture={
+                                          loadingAddProfilePicture
+                                        }
+                                      />
+                                    );
+                                  }}
+                                </AddProfilePictureComponent>
+                              );
+                            }}
+                          </SignS3Component>
+                        );
+                      }}
+                    </MeComponent>
+                    <Button
+                      bg="transparent"
+                      type="button"
+                      onClick={() => console.log("FILE INPUT CLICK")}
+                    >
+                      <Icon name="camera" size="2em" fill="crimson" />
+                    </Button>
+                  </Flex>
+                ) : (
+                  ""
+                )}
+              </Flex>
+              {expanded ? (
+                <Flex width={1 / 2} alignItems="center" flexDirection="column">
+                  <EditUserInfoFormBody
+                    handleSubmit={handleSubmit}
+                    values={values}
+                  />
+                </Flex>
+              ) : (
+                <Flex flexDirection="column">
+                  <Text fontFamily="main">
+                    {loadingMe
+                      ? "loading..."
+                      : dataMe && dataMe.me && dataMe.me.name}
+                  </Text>
+
+                  <Text>
+                    {loadingMe
+                      ? "loading..."
+                      : dataMe && dataMe.me && dataMe.me.email}
+                  </Text>
+                </Flex>
+              )}
             </Flex>
-          )}
-        </Flex>
-        <Button
-          type="button"
-          variant="outline"
-          onClick={() => setExpanded(!expanded)}
-        >
-          edit profile
-        </Button>
-      </>
+            <ButtonRow
+              expanded={expanded}
+              handleCancellation={() => setExpanded(!expanded)}
+              handleSubmit={handleSubmit}
+              loadingSubmitState={loadingSubmitState}
+              initialValues={initialFormValues}
+              resetForm={resetForm}
+              setExpanded={setExpanded}
+              setLoadingSubmitState={setLoadingSubmitState}
+            />
+          </Flex>
+        )}
+      </Formik>
     );
   } else {
     throw Error("Error! Data fetching states (Me) are all undefined!");
   }
 };
 
-interface UserInfoContainerProps {
-  dataMe: MeQueryResult["data"];
-  errorMe: MeQueryResult["error"];
-  loadingMe: MeQueryResult["loading"];
-}
+export type LoadingStateProps =
+  | "INITIAL_STATE"
+  | "IS_SUBMITTING"
+  | "IS_LOADING"
+  | "HAS_LOADED";
 
 const UserInfoContainer: React.FunctionComponent<UserInfoContainerProps> = ({
   dataMe,
   errorMe,
-  loadingMe
+  loadingMe,
+  editUserInfo,
+  dataEditUserInfo,
+  errorEditUserInfo,
+  loadingEditUserInfo
 }) => {
   const [expanded, setExpanded] = useState<boolean>(false);
+  const [loadingSubmitState, setLoadingSubmitState] = useState<
+    LoadingStateProps
+  >("INITIAL_STATE");
 
   return (
     <UserInfo
       dataMe={dataMe}
       errorMe={errorMe}
       loadingMe={loadingMe}
+      loadingSubmitState={loadingSubmitState}
       expanded={expanded}
       setExpanded={setExpanded}
+      setLoadingSubmitState={setLoadingSubmitState}
+      editUserInfo={editUserInfo}
+      dataEditUserInfo={dataEditUserInfo}
+      errorEditUserInfo={errorEditUserInfo}
+      loadingEditUserInfo={loadingEditUserInfo}
     />
   );
 };
