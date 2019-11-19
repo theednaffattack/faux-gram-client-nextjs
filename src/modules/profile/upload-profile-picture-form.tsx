8fc45cada698e0de8fe7280c1a6a5e219d5728e1
eddie naff <eddienaff@eddienaff.com>
2019-11-19 11:53:45 -0800

    Ditch Formik FieldArray for single image uploading

--- src/modules/profile/upload-profile-picture-form.tsx
+++ src/modules/profile/upload-profile-picture-form.tsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Formik, Field, FieldArray } from "formik";
+import { Formik, Field } from "formik";
 import * as Yup from "yup";
 
 import { InputField } from "../../components/fields/input-field";
 import {
   Button,
-  Card,
   Flex,
-  Icon,
-  Image,
   MaxFlex,
-  AbFlex
+  // Image,
+  FlexUserProfileWrap,
+  Avatar
+  // Card,
+  // Icon,
+  // AbFlex
 } from "../../components/styled-rebass";
 
 import { inputStyles } from "./dropzone";
-import { User } from "../../components/generated/apollo-graphql";
+import { User, MeQueryResult } from "../../components/generated/apollo-graphql";
+// import { value } from "popmotion";
 
 const UploadProfilePictureFormSchema = Yup.object().shape({
   images: Yup.array().required("Required")
 });
 
 export interface UploadProfilePictureFormProps {
+  dataMe: MeQueryResult["data"];
   files: any[];
   handleDrop?: any;
   handlePost: any;
@@ -42,13 +46,23 @@ export interface UploadProfilePictureFormProps {
   onDrop: any;
   fileInputRef: React.RefObject<HTMLInputElement>;
   onFilesAdded: any;
-  highlight: boolean;
+  highlight?: boolean;
   dataCreatePost: any;
   errorCreatePost: any;
   loadingCreatePost: any;
 }
 
+interface BlobUrlProps {
+  blobUrl: string;
+}
+
+interface InititalValuesObjProps {
+  images: BlobUrlProps[];
+  user: string;
+}
+
 function UploadProfilePictureForm({
+  dataMe,
   handlePost,
   userId,
   disabled,
@@ -57,23 +71,36 @@ function UploadProfilePictureForm({
   onDragLeave,
   onDrop,
   fileInputRef,
-  onFilesAdded,
-  highlight,
-  handleRemoveIndividualImagePreview,
-  handleClearFilePreview
+  // highlight,
+  // handleRemoveIndividualImagePreview,
+  // handleClearFilePreview
+  onFilesAdded
 }: UploadProfilePictureFormProps) {
+  const inititalValuesObj: InititalValuesObjProps = {
+    images: [],
+    user: userId
+  };
+  let profileImgUrl =
+    dataMe && dataMe.me && dataMe.me.profileImgUrl
+      ? dataMe.me.profileImgUrl
+      : "";
   return (
     <Formik
       validateOnBlur={false}
       validateOnChange={false}
       onSubmit={handlePost}
-      initialValues={{
-        images: [],
-        user: userId
-      }}
+      initialValues={inititalValuesObj}
       validationSchema={UploadProfilePictureFormSchema}
     >
-      {({ errors, handleSubmit, setFieldValue, touched, values }) => {
+      {({
+        errors,
+        handleSubmit,
+        setFieldValue,
+        touched,
+        values,
+        resetForm
+      }) => {
+        let areImagesVisisble = values.images.length > 0;
         return (
           <Flex width={1} flexDirection="column" alignItems="center">
             <form onSubmit={handleSubmit} style={{ width: "100%" }}>
@@ -83,19 +110,118 @@ function UploadProfilePictureForm({
                   flexDirection="column"
                   width={[1, 1, 1]}
                 >
+                  <MaxFlex
+                    alignItems="center"
+                    justifyContent="center"
+                    id="clickable-overlay"
+                    bg={areImagesVisisble ? "transparent" : "#fff"}
+                    width={"350px"}
+                    // border="2px dashed rgb(187, 186, 186)"
+                    border={
+                      areImagesVisisble || profileImgUrl !== ""
+                        ? "2px transparent solid"
+                        : "lime"
+                    }
+                    minHeight={"350px"}
+                    maxHeight={"350px"}
+                    style={{
+                      borderRadius: "50%",
+                      fontSize: "16px",
+                      cursor: disabled ? "default" : "pointer",
+                      overflow: "hidden"
+                    }}
+                    onClick={() => {
+                      // const { target, currentTarget } = event;
+                      // event.preventDefault();
+                      openFileDialog();
+                    }}
+                    onDragOver={onDragOver}
+                    onDragLeave={onDragLeave}
+                    onDrop={onDrop}
+                  >
+                    {values.images && values.images.length > 0 ? (
+                      <FlexUserProfileWrap
+                        height="300px"
+                        width="300px"
+                        overflow="hidden"
+                        borderRadius="50%"
+                        bg="thread_footer"
+                        alignItems="center"
+                        justifyContent="center"
+                        boxShadow="2px 2px 16px rgba(0, 0, 0, 0.25)"
+                      >
+                        <Avatar
+                          src={values.images[0].blobUrl}
+                          width={300}
+                          height={300}
+                        />
+                      </FlexUserProfileWrap>
+                    ) : (
+                      <FlexUserProfileWrap
+                        height="300px"
+                        width="300px"
+                        overflow="hidden"
+                        borderRadius="50%"
+                        bg="thread_footer"
+                        alignItems="center"
+                        justifyContent="center"
+                        boxShadow="2px 2px 16px rgba(0, 0, 0, 0.25)"
+                      >
+                        <Avatar src={profileImgUrl} width={300} height={300} />
+                      </FlexUserProfileWrap>
+                    )}
+
+                    {/* {values.images && values.images.length > 0 ? (
+                      <Image src={values.images[0].blobUrl} />
+                    ) : (
+                      ""
+                    )} */}
+                    <Field id="images" name="images">
+                      {() => {
+                        return (
+                          <input
+                            id="images"
+                            name="images"
+                            ref={fileInputRef}
+                            type="file"
+                            // onChange={onFilesAdded}
+                            // value={field.value}
+                            onChange={event => {
+                              if (
+                                event &&
+                                event.currentTarget &&
+                                event.currentTarget.files
+                              ) {
+                                let seeSomeFiles = onFilesAdded(event);
+
+                                setFieldValue(
+                                  "images",
+                                  values.images.concat(seeSomeFiles)
+                                );
+                              } else {
+                                return;
+                              }
+                            }}
+                            style={inputStyles}
+                            // multiple
+                          />
+                        );
+                      }}
+                    </Field>
+                  </MaxFlex>
+
+                  {/* 
                   <FieldArray
                     name="images"
-                    render={arrayHelpers => (
+                    render={() => (
                       <MaxFlex
                         id="clickable-overlay"
                         bg={highlight ? "rgb(188,185,236)" : "#fff"}
-                        width={values.images.length === 0 ? "350px" : 1}
+                        width={values.images.length === 0 ? "90px" : 1}
                         // border="2px dashed rgb(187, 186, 186)"
                         border={values.images.length === 0 ? "lime" : ""}
-                        minHeight={["350px"]}
-                        maxHeight={
-                          values.images.length === 0 ? "350px" : "auto"
-                        }
+                        minHeight={["90px"]}
+                        maxHeight={values.images.length === 0 ? "90px" : "auto"}
                         style={{
                           borderRadius: values.images.length === 0 ? "50%" : "",
                           fontSize: "16px",
@@ -142,7 +268,6 @@ function UploadProfilePictureForm({
                             );
                           }}
                         </Field>
-
                         <Flex flexDirection="column">
                           {values.images && values.images.length > 0 ? (
                             <Button
@@ -254,11 +379,11 @@ function UploadProfilePictureForm({
                                   )
                                 )
                               : ""}
-                          </Flex>
+                          </Flex> 
                         </Flex>
                       </MaxFlex>
                     )}
-                  />
+                  />*/}
                   {errors.images && touched.images ? (
                     <div>{errors.images}</div>
                   ) : null}
@@ -272,15 +397,27 @@ function UploadProfilePictureForm({
                 component={InputField}
               />
 
-              <Button type="submit">submit</Button>
+              <Button
+                disabled={!areImagesVisisble}
+                bg={areImagesVisisble ? "blue" : "grey"}
+                type="button"
+                onClick={() => handleSubmit()}
+              >
+                submit new image
+              </Button>
 
               <Button
                 bg={values.images.length < 1 ? "grey" : "blue"}
                 disabled={values.images.length < 1}
                 type="button"
-                onClick={handleClearFilePreview}
+                onClick={() =>
+                  resetForm({
+                    images: [],
+                    user: userId
+                  })
+                }
               >
-                Clear Preview
+                reset image
               </Button>
             </form>
           </Flex>
