dbf60502fb1c8b10a000d1c23a44e68fbc47d3c9
eddie naff <eddienaff@eddienaff.com>
2019-11-20 09:27:30 -0800

    Add new layout to pages

--- pages/change-password.tsx
+++ pages/change-password.tsx
@@ -2,6 +2,7 @@ import { Field, Formik } from "formik";
 import React from "react";
 import Router from "next/router";
 
+import Layout from "../src/modules/site-layout/layout";
 import { InputField } from "../src/components/fields/input-field";
 import {
   ChangePasswordComponent,
@@ -10,76 +11,78 @@ import {
 
 const ChangePassword = () => {
   return (
-    <HelloWorldComponent>
-      {() => (
-        <ChangePasswordComponent>
-          {changePassword => (
-            <Formik
-              // validateOnBlur={false}
-              // validateOnChange={false}
-              onSubmit={async (data, { setErrors }) => {
-                try {
-                  const response = await changePassword({
-                    variables: {
-                      data: {
-                        password: data.password,
-                        token: ""
+    <Layout>
+      <HelloWorldComponent>
+        {() => (
+          <ChangePasswordComponent>
+            {changePassword => (
+              <Formik
+                // validateOnBlur={false}
+                // validateOnChange={false}
+                onSubmit={async (data, { setErrors }) => {
+                  try {
+                    const response = await changePassword({
+                      variables: {
+                        data: {
+                          password: data.password,
+                          token: ""
+                        }
                       }
-                    }
-                  });
+                    });
 
-                  console.log({ response });
+                    console.log({ response });
 
-                  Router.push("/");
-                } catch (error) {
-                  const displayErrors: { [key: string]: string } = {};
+                    Router.push("/");
+                  } catch (error) {
+                    const displayErrors: { [key: string]: string } = {};
 
-                  let myErrors =
-                    error.graphQLErrors[0].extensions.exception
-                      .validationErrors;
+                    let myErrors =
+                      error.graphQLErrors[0].extensions.exception
+                        .validationErrors;
 
-                  myErrors.forEach((validationError: any) => {
-                    Object.values(validationError.constraints).forEach(
-                      (message: any) => {
-                        displayErrors[validationError.property] = message;
-                      }
-                    );
-                  });
-                  return setErrors(displayErrors);
+                    myErrors.forEach((validationError: any) => {
+                      Object.values(validationError.constraints).forEach(
+                        (message: any) => {
+                          displayErrors[validationError.property] = message;
+                        }
+                      );
+                    });
+                    return setErrors(displayErrors);
 
-                  // const errors: { [key: string]: string } = {};
-                  // err.graphQLErrors[0].validationErrors.forEach(
-                  //   (validationErr: any) => {
-                  //     Object.values(validationErr.constraints).forEach(
-                  //       (message: any) => {
-                  //         errors[validationErr.property] = message;
-                  //       }
-                  //     );
-                  //   }
-                  // );
-                  // setErrors(errors);
-                }
-              }}
-              initialValues={{
-                password: ""
-              }}
-            >
-              {({ handleSubmit }) => (
-                <form onSubmit={handleSubmit}>
-                  <Field
-                    name="password"
-                    type="password"
-                    placeholder="change password"
-                    component={InputField}
-                  />
-                  <button type="submit">change password</button>
-                </form>
-              )}
-            </Formik>
-          )}
-        </ChangePasswordComponent>
-      )}
-    </HelloWorldComponent>
+                    // const errors: { [key: string]: string } = {};
+                    // err.graphQLErrors[0].validationErrors.forEach(
+                    //   (validationErr: any) => {
+                    //     Object.values(validationErr.constraints).forEach(
+                    //       (message: any) => {
+                    //         errors[validationErr.property] = message;
+                    //       }
+                    //     );
+                    //   }
+                    // );
+                    // setErrors(errors);
+                  }
+                }}
+                initialValues={{
+                  password: ""
+                }}
+              >
+                {({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Field
+                      name="password"
+                      type="password"
+                      placeholder="change password"
+                      component={InputField}
+                    />
+                    <button type="submit">change password</button>
+                  </form>
+                )}
+              </Formik>
+            )}
+          </ChangePasswordComponent>
+        )}
+      </HelloWorldComponent>
+    </Layout>
   );
 };
 
