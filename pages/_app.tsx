5bc41de603946593f846ca1039a4e77ac1e7dd2b
eddie naff <eddienaff@eddienaff.com>
2019-11-20 12:40:39 -0800

    Remove layout component from _app use getLayout instead

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,18 @@ import { theme } from "../src/styles/theme";
 import withApollo from "../src/lib/withApollo";
 // import "./empty.css";
 import { GlobalStyles } from "../src/styles/global-styles";
-import Layout from "../src/modules/site-layout/layout";
 
 class MyApp extends App<any> {
   render() {
     // @ts-ignore
     const { Component, pageProps, apolloClient, router } = this.props;
     const getLayout = Component.getLayout || ((page: any) => page);
+    const title = Component.title || "Fake test title";
     return (
       <ThemeProvider theme={theme}>
         <ApolloProvider client={apolloClient}>
           <GlobalStyles />
-          <Layout title="A title">
-            {getLayout(<Component {...pageProps} />)}
-          </Layout>
+          {getLayout(<Component title={title} {...pageProps} />)}
         </ApolloProvider>
       </ThemeProvider>
     );
