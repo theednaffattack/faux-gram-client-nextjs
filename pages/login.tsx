e809df0ae14f7fffb3c932d05dc24d330d0437ce
eddie naff <eddienaff@eddienaff.com>
2019-11-20 13:27:55 -0800

    Add getInitialProps

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,32 +1,31 @@
 import Login from "../src/modules/auth/login";
 import { MyContext } from "../types/types";
-import Layout, { getLayout } from "../src/modules/site-layout/layout";
+import { getLayout } from "../src/modules/site-layout/layout";
 
 interface LoginProps {
   referer: MyContext["referer"];
 }
 
-interface IIndexPage {
+interface ILoginPage {
   ({ referer }: LoginProps): JSX.Element;
+
+  getInitialProps: ({
+    referer
+  }: MyContext) => Promise<{ referer: MyContext["referer"] }>;
 
   getLayout: (page: any) => JSX.Element;
 
   title: string;
 }
 
-const LoginPage: IIndexPage = ({ referer }) => (
-  <Layout title="Login">
-    <Login referer={referer} />
-  </Layout>
-);
+const LoginPage: ILoginPage = ({ referer }) => <Login referer={referer} />;
 
-// @ts-ignore
 LoginPage.getInitialProps = async ({ referer }: MyContext) => {
   let setReferer = referer === undefined ? "/login" : referer;
   return { referer: setReferer };
 };
 
 LoginPage.getLayout = getLayout;
-LoginPage.title = "Home";
+LoginPage.title = "Login";
 
 export default LoginPage;
