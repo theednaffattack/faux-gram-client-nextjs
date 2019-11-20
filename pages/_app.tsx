cb96d0dc4d378e801ccecb97502a2abfd9344f1f
eddie naff <eddienaff@eddienaff.com>
2019-11-20 09:25:20 -0800

    Change layout import and add getLayout method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,19 @@ import { theme } from "../src/styles/theme";
 import withApollo from "../src/lib/withApollo";
 // import "./empty.css";
 import { GlobalStyles } from "../src/styles/global-styles";
-import Layout from "../src/components/layout";
+import Layout from "../src/modules/site-layout/layout";
 
 class MyApp extends App<any> {
   render() {
-    const { Component, pageProps, apolloClient } = this.props;
+    // @ts-ignore
+    const { Component, pageProps, apolloClient, router } = this.props;
+    const getLayout = Component.getLayout || ((page: any) => page);
     return (
       <ThemeProvider theme={theme}>
         <ApolloProvider client={apolloClient}>
           <GlobalStyles />
           <Layout title="A title">
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
           </Layout>
         </ApolloProvider>
       </ThemeProvider>
