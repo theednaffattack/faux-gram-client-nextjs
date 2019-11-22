075eceb59ed788343238deb04b039f60f8083a37
eddie naff <eddienaff@eddienaff.com>
2019-11-21 22:14:50 -0800

    Remove error laden video component

--- src/modules/feed/camera_v1/video.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { PossibleMediaStream, useUserMedia } from "./useUserMedia";
-
-const Video = React.forwardRef((CAPTURE_OPTIONS, ref) => {
-  let mediaStream: PossibleMediaStream = useUserMedia(CAPTURE_OPTIONS);
-
-  useFunctionality(ref);
-  return null;
-});
