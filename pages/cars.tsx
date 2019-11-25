992a30cc5847abbf36b680789bd3d6ccc054ecf5
eddie naff <eddienaff@eddienaff.com>
2019-11-24 22:21:48 -0800

    fix(cars): import react to fix lint error

--- pages/cars.tsx
+++ pages/cars.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+
 import CarsOverview from "../src/modules/cars/Overview";
 import { getLayout } from "../src/modules/site-layout/layout";
 
