45f5740367d7bfdaf996af314eeb248ac8f95eeb
eddie naff <eddienaff@eddienaff.com>
2019-11-26 17:39:23 -0800

    refactor: add formik form directly rather than importing it as a comp
    Refactor the component to no longer import the form. Instead I applied formik directly, for now.

--- src/modules/post/create-post-mutation.tsx
+++ src/modules/post/create-post-mutation.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import Axios from "axios";
+import { Formik, Form, Field } from "formik";
+// import * as Yup from "yup";
+import Nope from "nope-validator";
 
 import {
   CreatePostComponent,
   SignS3Component
-  // CreatePostMutationVariables,
-  // SignedS3SubPayload,
-  // SignS3MutationVariables,
-  // Image,
-  // Post
 } from "../../components/generated/apollo-graphql";
-// import { ApolloError } from "apollo-client";
-// import { MutationFunction } from "@apollo/react-common";
+import { Button, AbFlex } from "../../components/styled-rebass";
+import { TextFormField } from "./text-form-field";
+
+// const UserSchema = Nope.object().shape({
+//   name: Nope.string()
+//     .min(5, "Please provide a longer name")
+//     .max(255, "Name is too long!"),
+//   email: Nope.string()
+//     .email()
+//     .required(),
+//   confirmEmail: Nope.string()
+//     .oneOf([Nope.ref("email")])
+//     .requried()
+// });
+
+const PostSchema = Nope.object().shape({
+  text: Nope.string()
+    .required()
+    .min(2),
+  title: Nope.string()
+});
+
+// const PostSchema = Yup.object({
+//   text: Yup.string()
+//     .required()
+//     .min(2),
+//   title: Yup.string()
+// });
 
 interface IFileListMutation {
   me: string;
@@ -19,6 +43,17 @@ interface IFileListMutation {
 }
 
 const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
+  const makeBlobUrlsFromReference = async (myFile: any) => {
+    return await fetch(myFile)
+      .then(r => r.blob())
+      .then(blobFile => {
+        const getFileName = "tempName";
+
+        return new File([blobFile], getFileName, {
+          type: blobFile.type
+        });
+      });
+  };
   const uploadToS3 = async ({ file, signedRequest }: any) => {
     const options = {
       headers: {
@@ -61,31 +96,49 @@ const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
                 loading: loadingCreatePost
               }
             ) => {
+              console.log({ dataCreatePost, errorCreatePost });
               return (
                 <div
                   style={{
                     display: "flex",
-
+                    padding: "16px",
                     flexDirection: "column",
-                    border: "2px crimson solid"
+                    border: "1px #ccc solid",
+                    marginTop: "8px",
+                    width: "640px",
+                    position: "relative"
                   }}
                 >
+                  {loadingCreatePost ? (
+                    <AbFlex
+                      position="absolute"
+                      top={0}
+                      left={0}
+                      right={0}
+                      bottom={0}
+                      bg="rgba(255,255,255,0.7)"
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      WEIRD
+                    </AbFlex>
+                  ) : (
+                    ""
+                  )}
                   {loadingSignS3 ? "contacting S3" : ""}
-                  <button
-                    onClick={async () => {
+
+                  <Formik
+                    validationSchema={PostSchema}
+                    initialValues={{
+                      text: "",
+                      title: "",
+                      user: me
+                    }}
+                    onSubmit={async ({ user, text, title }) => {
                       let getVariables = await makeBlobUrlsFromReference(
                         cardImage
                       );
-                      // console.log("VIEW getVariables ", {
-                      //   cardImage,
-                      //   getVariables: {
-                      //     filename: getVariables.name,
-                      //     filetype: getVariables.type,
-                      //     lastModified: getVariables.lastModified,
-                      //     size: getVariables.size
-                      //   },
-                      //   signS3
-                      // });
+
                       let s3SignatureResponse = await signS3({
                         variables: {
                           files: [
@@ -96,11 +149,8 @@ const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
                           ]
                         }
                       });
-                      if (s3SignatureResponse && s3SignatureResponse.data) {
-                        console.log("VIEW s3SignatureResponse", {
-                          s3SignatureResponse
-                        });
 
+                      if (s3SignatureResponse && s3SignatureResponse.data) {
                         let getUrl = await uploadToS3({
                           file: cardImage,
                           signedRequest:
@@ -115,9 +165,9 @@ const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
                                 s3SignatureResponse.data.signS3.signatures[0]
                                   .url
                               ],
-                              text: "some text",
-                              title: "a title",
-                              user: me
+                              text,
+                              title,
+                              user
                             }
                           }
                         });
@@ -129,8 +179,28 @@ const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
                       }
                     }}
                   >
-                    Sign S3 Upload to S3
-                  </button>
+                    {({ errors }) => {
+                      console.log("VIEW ERRORS", { errors });
+                      return (
+                        <Form>
+                          <Field name="user" hidden component={TextFormField} />
+                          <Field
+                            name="text"
+                            label="Text"
+                            placeholder="Propel ships"
+                            component={TextFormField}
+                          />
+                          <Field
+                            name="title"
+                            label="Title"
+                            placeholder="Fuel trucks"
+                            component={TextFormField}
+                          />
+                          <Button mt={3}>Sign S3 Upload to S3</Button>
+                        </Form>
+                      );
+                    }}
+                  </Formik>
                 </div>
               );
             }}
@@ -140,17 +210,5 @@ const CreatePostMutation = ({ cardImage, me }: IFileListMutation) => {
     </SignS3Component>
   );
 };
-
-async function makeBlobUrlsFromReference(myFile: any) {
-  return await fetch(myFile)
-    .then(r => r.blob())
-    .then(blobFile => {
-      const getFileName = "tempName";
-
-      return new File([blobFile], getFileName, {
-        type: blobFile.type
-      });
-    });
-}
 
 export default CreatePostMutation;
