7a0836e51d44a24fee09ef8e68b9a7e85075a58b
eddie naff <eddienaff@eddienaff.com>
2019-11-29 19:50:50 -0800

    fix: fix server production bug  (won't submit)

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,7 @@ import next from "next";
 import fs from "fs";
 import path from "path";
 import { createServer } from "https";
-// import { createServer } from "http";
+import http from "http";
 // import internalIp from "internal-ip";
 
 const nodeEnv = process.env.NODE_ENV;
@@ -24,31 +24,67 @@ const domain = dev ? localHostIpAddress : productionDomain;
 
 const prefix = "https://";
 
-const certOptions = {
-  key: fs.readFileSync(path.resolve("private/server.key")),
-  cert: fs.readFileSync(path.resolve("private/server.crt"))
-};
+interface CertOptions {
+  key: Buffer;
+  cert: Buffer;
+}
+
+let certOptions: CertOptions;
+
+if (dev) {
+  certOptions = {
+    key: fs.readFileSync(path.resolve("private/server.key")),
+    cert: fs.readFileSync(path.resolve("private/server.crt"))
+  };
+}
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
-app.prepare().then(() => {
-  createServer(certOptions, (req, res) => {
-    const parsedUrl: UrlWithParsedQuery = parse(req.url!, true);
-    const { pathname, query } = parsedUrl;
 
-    if (pathname === "/a") {
-      app.render(req, res, "/a", query);
-    } else if (pathname === "/b") {
-      app.render(req, res, "/b", query);
-    } else {
-      // handle(req, res, parsedUrl);
+if (!dev) {
+  app.prepare().then(() => {
+    createServer(certOptions, (req, res) => {
+      const parsedUrl: UrlWithParsedQuery = parse(req.url!, true);
+      const { pathname, query } = parsedUrl;
 
-      handle(req, res);
-    }
-  }).listen(port);
+      if (pathname === "/a") {
+        app.render(req, res, "/a", query);
+      } else if (pathname === "/b") {
+        app.render(req, res, "/b", query);
+      } else {
+        // handle(req, res, parsedUrl);
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `\n🚀 Server listening at ${prefix}${domain}:${port} as ${nodeEnv}\n`
-  );
-});
+        handle(req, res);
+      }
+    }).listen(port);
+
+    // tslint:disable-next-line:no-console
+    console.log(
+      `\n🚀 Server listening at ${prefix}${domain}:${port} as ${nodeEnv}\n`
+    );
+  });
+} else {
+  app.prepare().then(() => {
+    http
+      .createServer((req, res) => {
+        const parsedUrl: UrlWithParsedQuery = parse(req.url!, true);
+        const { pathname, query } = parsedUrl;
+
+        if (pathname === "/a") {
+          app.render(req, res, "/a", query);
+        } else if (pathname === "/b") {
+          app.render(req, res, "/b", query);
+        } else {
+          // handle(req, res, parsedUrl);
+
+          handle(req, res);
+        }
+      })
+      .listen(port);
+
+    // tslint:disable-next-line:no-console
+    console.log(
+      `\n🚀 Server listening at ${prefix}${domain}:${port} as ${nodeEnv}\n`
+    );
+  });
+}
