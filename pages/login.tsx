e0bdba781a5181fc642d3aa26254dfacf4858698
eddie naff <eddienaff@eddienaff.com>
2019-12-01 19:07:46 -0800

    feat: change referer for testing

--- pages/login.tsx
+++ pages/login.tsx
@@ -23,7 +23,7 @@ interface ILoginPage {
 const LoginPage: ILoginPage = ({ referer }) => <Login referer={referer} />;
 
 LoginPage.getInitialProps = async ({ referer }: MyContext) => {
-  let setReferer = referer === undefined ? "/unknown page" : referer;
+  let setReferer = referer === undefined ? "/the login page" : referer;
   return { referer: setReferer };
 };
 
