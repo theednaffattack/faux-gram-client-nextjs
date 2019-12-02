8720ee492a40874f940a3adb6336591bc1fca754
eddie naff <eddienaff@eddienaff.com>
2019-12-01 19:11:22 -0800

    feat: add bottom spacer div

---
+++ src/modules/feed/camera/layout-foot-spacer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface LayoutFootSpacerProps {}
+
+const LayoutFootSpacer: React.FunctionComponent<LayoutFootSpacerProps> = () => {
+  return (
+    <div
+      style={{
+        width: "25px",
+        height: "67px"
+      }}
+    ></div>
+  );
+};
+
+export default LayoutFootSpacer;
